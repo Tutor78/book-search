a5e356e7fb6a8f6b0d13250751aaeac73f137497
Michael Tutor <tutor.michael78@gmail.com>
2021-11-23 13:58:00 -0600

    added queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from '@apollo/client';
+
+export const GET_ME = gql`
+    query me {
+        me {
+            _id
+            username
+            bookCount
+            savedBooks {
+                _id
+                bookId
+                title
+                authors
+                description
+                image
+            }
+        }
+    }
+`;
