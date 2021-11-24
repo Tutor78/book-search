6660b7d0afc789933579d2bb2214f7aff36dd148
Michael Tutor <tutor.michael78@gmail.com>
2021-11-23 19:06:44 -0600

    fixed typo

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -55,7 +55,7 @@ export const SAVE_BOOK = gql`
 `;
 
 export const REMOVE_BOOK = gql`
-    mutation deleteBook($bookId: ID!) {
+    mutation removeBook($bookId: ID!) {
         removeBook(bookId: $bookId) {
             username
             savedBooks {
