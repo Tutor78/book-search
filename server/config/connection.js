fb92c2939a252a95c86c6a640995f9f8c142476e
Tutor <tutor.michael78@gmail.com>
2021-11-24 20:15:45 -0600

    another attempt

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
+  useUnifiedTopology: true
 });
 
 module.exports = mongoose.connection;
